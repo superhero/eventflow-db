463aacd31298fedcb1ff9491c525b0df44b9bf23
Erik Landvall <erik@landvall.se>
2025-03-21 20:51:10 +0100

    attempting to resolve econrefused bug in mysql connection pool

--- config.js
+++ config.js
@@ -11,18 +11,21 @@ export default
   {
     db:
     {
-      debug           : process.env.EVENTFLOW_MYSQL_DEBUG             ?? false,
-      insecureAuth    : process.env.EVENTFLOW_MYSQL_INSECURE_AUTH     ?? false,
-      enableKeepAlive : process.env.EVENTFLOW_MYSQL_ENABLE_KEEP_ALIVE ?? true,
-      connectionLimit : process.env.EVENTFLOW_MYSQL_CONNECTION_LIMIT  ?? '5',
-      flags           : process.env.EVENTFLOW_MYSQL_FLAGS             ?? '',
-      charset         : process.env.EVENTFLOW_MYSQL_CHARSET           ?? 'UTF8_GENERAL_CI',
-      timezone        : process.env.EVENTFLOW_MYSQL_TIMEZONE          ?? 'Z',
-      host            : process.env.EVENTFLOW_MYSQL_HOST              ?? 'localhost',
-      port            : process.env.EVENTFLOW_MYSQL_PORT              ?? '3306',
-      user            : process.env.EVENTFLOW_MYSQL_USER              ?? 'root',
-      password        : process.env.EVENTFLOW_MYSQL_PASS              ?? 'root',
-      database        : process.env.EVENTFLOW_MYSQL_DB                ?? 'eventflow',
+      debug                 : process.env.EVENTFLOW_MYSQL_DEBUG             ?? false,
+      insecureAuth          : process.env.EVENTFLOW_MYSQL_INSECURE_AUTH     ?? false,
+      enableKeepAlive       : process.env.EVENTFLOW_MYSQL_ENABLE_KEEP_ALIVE ?? true,
+      waitForConnections    : process.env.EVENTFLOW_MYSQL_WAIT_FOR_CONN     ?? true,
+      keepAliveInitialDelay : process.env.EVENTFLOW_MYSQL_KEEP_ALIVE_DELAY  ?? '10000',
+      queueLimit            : process.env.EVENTFLOW_MYSQL_QUEUE_LIMIT       ?? '0',
+      connectionLimit       : process.env.EVENTFLOW_MYSQL_CONNECTION_LIMIT  ?? '5',
+      flags                 : process.env.EVENTFLOW_MYSQL_FLAGS             ?? '',
+      charset               : process.env.EVENTFLOW_MYSQL_CHARSET           ?? 'UTF8_GENERAL_CI',
+      timezone              : process.env.EVENTFLOW_MYSQL_TIMEZONE          ?? 'Z',
+      host                  : process.env.EVENTFLOW_MYSQL_HOST              ?? 'localhost',
+      port                  : process.env.EVENTFLOW_MYSQL_PORT              ?? '3306',
+      user                  : process.env.EVENTFLOW_MYSQL_USER              ?? 'root',
+      password              : process.env.EVENTFLOW_MYSQL_PASS              ?? 'root',
+      database              : process.env.EVENTFLOW_MYSQL_DB                ?? 'eventflow',
     }
   }
 }
